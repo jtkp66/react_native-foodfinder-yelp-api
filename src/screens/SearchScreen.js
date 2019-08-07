db64a984bd6abeee86e163c0d990561d49a36e16
Jamie Pham <jtkp66@gmail.com>
2019-08-07 11:59:11 -0700

    Adding searchbar component to searchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreeen = () => {
   return (
     <View>
+      <SearchBar />
       <Text>SearchScreeen</Text>
     </View>
   );
