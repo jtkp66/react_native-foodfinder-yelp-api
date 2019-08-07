160ba99fea55146a41a702fde6e14e09cea8af10
Jamie Pham <jtkp66@gmail.com>
2019-08-06 21:23:07 -0700

    initial commit with custom homescreen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreeen = () => {
+  return (
+    <View>
+      <Text>SearchScreeen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreeen;
