72d1753ef5c7db7d7db0331939c2e2280e720f25
Jamie Pham <jtkp66@gmail.com>
2019-08-07 15:12:18 -0700

    Added ResultsList component and rendering restaurants from api call in a flatlist

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreeen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMesage] = useResults();
+
+  const filterResultsByPrice = price => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -16,6 +23,9 @@ const SearchScreeen = () => {
       />
       {errorMesage ? <Text>{errorMesage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 };
